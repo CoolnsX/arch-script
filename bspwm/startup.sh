16494e1f29134a82761104899ff8cb4423ec7db6
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-06-10 10:29:29 +0530

    Add files via upload

--- bspwm/startup.sh
+++ bspwm/startup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-killall -q nm-applet
-while pgrep -x nm-applet >/dev/null; do sleep 1; done
-nm-applet &
+sleep 5
+synclient TapButton1=1
+synclient HorizEdgeScroll=1
+synclient RBCornerButton=3
 
-killall -q blueman-applet
-while pgrep -x blueman-applet >/dev/null; do sleep 1; done
-blueman-applet &
