2263ee0dd1351770e515ca183b8c9ef98d65edca
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-07-26 17:09:51 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -8,17 +8,18 @@ git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 
-yay -S --noconfirm auto-cpufreq polybar ttf-unifont
-sudo systemctl enable --now auto-cpufreq
-
 echo "MAIN PACKAGES"
 
 sleep 5
 
 sudo pacman -S --noconfirm --needed xorg-server slock lightdm bspwm sxhkd st picom xwallpaper lxappearance-gtk3 dmenu pcmanfm simplescreenrecorder alsa-utils pulseaudio pulseaudio-alsa pavucontrol vlc dunst
 
+yay -S --noconfirm polybar ttf-unifont google-chrome mugshot qogir-gtk-theme qogir-icon-theme auto-cpufreq timeshift timeshift-autosnap ttf-ms-fonts
+sudo systemctl enable --now auto-cpufreq
+
 sudo systemctl enable lightdm
 
+git clone https://github.com/CoolnsX/repos_scripts.git
 git clone https://github.com/CoolnsX/dotfiles.git
 cd dotfiles
 
