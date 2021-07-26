962afcfb9851e67a974ab0f473c37f2f0f2ffd22
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-07-26 16:52:10 +0530

    Update install-mbr.sh

--- install-mbr.sh
+++ install-mbr.sh
@@ -5,7 +5,7 @@ hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "Arch" >> /etc/hostname
+echo "ArchLen" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
@@ -15,7 +15,7 @@ passwd root
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --noconfirm grub grub-btrfs networkmanager btrfs-progs network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nano
+pacman -S --noconfirm grub grub-btrfs networkmanager btrfs-progs dialog wpa_supplicant dosfstools reflector base-devel xdg-user-dirs xdg-utils gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nvim
 pacman -S --noconfirm xf86-video-intel
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
@@ -31,7 +31,7 @@ passwd tanveer
 
 echo "Enable users wheel in sudoers file"
 sleep 5
-EDITOR=nano visudo
+EDITOR=nvim visudo
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.If u installed system on btrfs... considering add btrfs in modules of mkinitcpio file and generate initcpio file\e[0m"
 
