3b754098c8f4f356f9e7885ba51f4b3372abff70
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-08-03 15:53:58 +0530

    add doas program

--- install-uefi.sh
+++ install-uefi.sh
@@ -15,8 +15,8 @@ passwd root
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --noconfirm efibootmgr os-prober grub grub-btrfs networkmanager btrfs-progs network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nano
-pacman -S --noconfirm xf86-video-intel
+pacman -S --noconfirm --needed doas efibootmgr os-prober grub grub-btrfs networkmanager btrfs-progs network-manager-applet dialog wpa_supplicant dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nano
+pacman -S --noconfirm --needed xf86-video-intel
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
@@ -29,9 +29,12 @@ systemctl enable sshd
 useradd -mG wheel tanveer
 passwd tanveer
 
-echo "Enable users wheel in sudoers file"
-sleep 5
-EDITOR=nano visudo
+touch /etc/doas.conf
+echo "permit nopass :wheel as root" >> /etc/doas.conf
+
+#echo "Enable users wheel in sudoers file"
+#sleep 5
+#EDITOR=nano visudo
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.If u installed system on btrfs FS... considering add btrfs in modules of mkinitcpio file and generate initcpio file\e[0m"
 
