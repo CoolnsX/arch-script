f65103af2e12451d46b19ffe4ac251252c15f1c2
CoolnsX <76195824+CoolnsX@users.noreply.github.com>
2021-08-03 15:51:56 +0530

    add doas option

--- install-mbr.sh
+++ install-mbr.sh
@@ -15,8 +15,8 @@ passwd root
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S --noconfirm grub grub-btrfs networkmanager btrfs-progs dialog wpa_supplicant dosfstools reflector base-devel xdg-user-dirs xdg-utils gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nvim
-pacman -S --noconfirm xf86-video-intel
+pacman -S --noconfirm --needed grub doas grub-btrfs networkmanager btrfs-progs dialog wpa_supplicant dosfstools reflector base-devel xdg-user-dirs xdg-utils gvfs-mtp bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth bash-completion openssh git nvim
+pacman -S --noconfirm --needed xf86-video-intel
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
@@ -29,9 +29,12 @@ systemctl enable sshd
 useradd -mG wheel tanveer
 passwd tanveer
 
-echo "Enable users wheel in sudoers file"
-sleep 5
-EDITOR=nvim visudo
+touch /etc/doas.conf
+echo "permit nopass :wheel as root" >> /etc/doas.conf
+
+#echo "Enable users wheel in sudoers file"
+#sleep 5
+#EDITOR=nvim visudo
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.If u installed system on btrfs... considering add btrfs in modules of mkinitcpio file and generate initcpio file\e[0m"
 
